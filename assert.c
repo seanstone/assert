a3f4b6f73114f71c3091362b0016b068cb65fddd
En Shih <seanstone5923@gmail.com>
2017-01-21 22:34:39 +0800

    Allow function calls in assert_constant()

--- assert.c
+++ assert.c
@@ -11,16 +11,22 @@
 #define assert_constant(expression) \
 ({\
     __attribute__((error("\e[33;40;1m""\""#expression"\" is not a constant expression""\e[0m"))) void assert_constant_error();\
-    if (!(__builtin_constant_p(expression)))\
+    int i = expression;\
+    if (!(__builtin_constant_p(i)))\
         assert_constant_error();\
     expression;\
 })
+
+int square (int i)
+{
+    return i * i;
+}
 
 int main (void)
 {
     int r = 5;
     scanf("%d", &r); /* this causes the assert constant error */
-    int d = assert_constant(r);
+    int d = assert_constant(square(r));
     printf("%d\n", d);
 
     return 0;
