31ca9bcb581ce607ce53c0f5508575a08db2bdb0
En Shih <seanstone5923@gmail.com>
2017-01-21 21:01:12 +0800

    assert_constant() now returns original expression

--- assert.c
+++ assert.c
@@ -9,17 +9,19 @@
 }
 
 #define assert_constant(expression) \
-{\
+({\
     __attribute__((error("\e[33;40;1m""\""#expression"\" is not a constant expression""\e[0m"))) void assert_constant_error();\
     if (!(__builtin_constant_p(expression)))\
         assert_constant_error();\
-}
+    expression;\
+})
 
 int main (void)
 {
-    int r = 0;
+    int r = 5;
     scanf("%d", &r); /* this causes the assert constant error */
-    assert_constant(r);
+    int d = assert_constant(r);
+    printf("%d\n", d);
 
     return 0;
 }
