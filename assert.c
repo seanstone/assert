3fc4166b9ff13b5b104ebff1759d7943f564bd96
En Shih <seanstone5923@gmail.com>
2017-01-21 22:39:16 +0800

    Change colors

--- assert.c
+++ assert.c
@@ -3,14 +3,14 @@
 
 #define static_assert(condition, message) \
 {\
-    __attribute__((error("\e[33;40;1m"message"\e[0m"))) void static_assert_error();\
+    __attribute__((error("\e[37;41;1m"message"\e[0m"))) void static_assert_error();\
     if (!(condition))\
         static_assert_error();\
 }
 
 #define assert_constant(expression) \
 ({\
-    __attribute__((error("\e[33;40;1m""\""#expression"\" is not a constant expression""\e[0m"))) void assert_constant_error();\
+    __attribute__((error("\e[37;41;1m""\""#expression"\" is not a constant expression""\e[0m"))) void assert_constant_error();\
     int i = expression;\
     if (!(__builtin_constant_p(i)))\
         assert_constant_error();\
